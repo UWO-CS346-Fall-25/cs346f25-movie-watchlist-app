97ae63faa4b7b45822d84744e507de111b49caa0
Sam <sreynebeau11@gmail.com>
2025-10-23 18:07:23 -0500

    fixed server bug, was being called to start twice. cleaned up ReadMe.

--- src/app.js
+++ src/app.js
@@ -44,10 +44,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
