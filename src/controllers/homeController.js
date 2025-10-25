6f7f6c1b942270bb5dbc5ca446bfaa2a5784dab4
Damian Johnson <127550510+Damian-Johnson@users.noreply.github.com>
2025-10-25 09:47:18 -0500

    Feature/week8 interactivity (#2)
    * More js add ins + better structure for future database implementation
    
    * fixed server bug, was being called to start twice. cleaned up ReadMe.
    
    ---------
    
    Co-authored-by: Sam <sreynebeau11@gmail.com>

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,26 +2,36 @@
  * Home Controller
  *
  * Contains methods for handling:
- * - Home page
- * - History page
+ * - Home page with movie data
+ * - History page with watched movies
  * - Settings page
  */
 
+const movieModel = require('../models/movieModel');
+
 // Home page controller
 exports.getHome = (req, res) => {
+  const movies = movieModel.getAllWatchlistMovies();
+
   res.render('index', {
     title: 'Home',
-    user: req.user,
-    csrfToken: req.csrfToken(),
+    user: req.user || null,
+    csrfToken: req.csrfToken ? req.csrfToken() : '',
+    movies: movies,
+    totalMovies: movies.length,
   });
 };
 
 // History page controller
 exports.getHistory = (req, res) => {
+  const watchedMovies = movieModel.getAllWatchedMovies();
+
   res.render('history', {
     title: 'History',
-    user: req.user,
-    csrfToken: req.csrfToken(),
+    user: req.user || null,
+    csrfToken: req.csrfToken ? req.csrfToken() : '',
+    watchedMovies: watchedMovies,
+    totalWatched: watchedMovies.length,
   });
 };
 
@@ -29,7 +39,8 @@ exports.getHistory = (req, res) => {
 exports.getSettings = (req, res) => {
   res.render('settings', {
     title: 'Settings',
-    user: req.user,
-    csrfToken: req.csrfToken(),
+    user: req.user || null,
+    csrfToken: req.csrfToken ? req.csrfToken() : '',
+    currentTheme: 'light',
   });
 };
---
+++ src/routes/api.js
@@ -0,0 +1,30 @@
+/**
+ * API Routes
+ *
+ * Contains API endpoints for AJAX operations:
+ * - GET, POST, PUT, DELETE for movies
+ */
+
+const express = require('express');
+const router = express.Router();
+const movieController = require('../controllers/movieController');
+
+// Get all movies in watchlist
+router.get('/movies', movieController.getMovies);
+
+// Get all watched movies
+router.get('/watched', movieController.getWatchedMovies);
+
+// Add a new movie
+router.post('/movies', movieController.addMovie);
+
+// Mark movie as watched
+router.put('/movies/:id/watched', movieController.markAsWatched);
+
+// Remove movie
+router.delete('/movies/:id', movieController.removeMovie);
+
+// Update movie review
+router.put('/watched/:id/review', movieController.updateReview);
+
+module.exports = router;
